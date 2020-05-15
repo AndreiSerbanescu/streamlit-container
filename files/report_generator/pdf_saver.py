e819da9b02b5fe255628b56c01123e0d0f8c1500
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-05-15 16:26:28 +0300

    remove files besides pdf report when report generation is completed

--- files/report_generator/pdf_saver.py
+++ files/report_generator/pdf_saver.py
@@ -2,21 +2,28 @@ import os
 from common.utils import *
 import subprocess as sb
 
+
 class Markdown2Pdf:
 
-    def __init__(self, report_dir):
+    def __init__(self, report_dir, remove_files=True):
 
         self.report_dir = report_dir
+        self.report_name = "report.pdf"
+        self.remove_files = remove_files
 
     def generate_pdf(self):
-
-        markdown_filename = os.path.join(self.report_dir, "report.md")
-        pdf_filename      = os.path.join(self.report_dir, "report.pdf")
-
-        pandoc_cmd = f"pandoc -s -o {pdf_filename} {markdown_filename}"
+        pandoc_cmd = f"cd {self.report_dir} && pandoc -s -o {self.report_name} report.md"
 
         sb.check_output([pandoc_cmd], shell=True)
+        pdf_filename = os.path.join(self.report_dir, self.report_name)
+
+        if self.remove_files:
+            self.__remove_other_files()
 
         return pdf_filename
 
-
+    def __remove_other_files(self):
+        files = os.listdir(self.report_dir)
+        for file in files:
+            if file != self.report_name:
+                os.remove(os.path.join(self.report_dir, file))
